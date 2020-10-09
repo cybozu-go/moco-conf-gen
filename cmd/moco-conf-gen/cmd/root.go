5e8744ee49fecdbec23bad1ea8add80e175e9a69
Kazuhito MATSUDA <kazuhito.matuda@gmail.com>
2020-10-09 11:47:15 +0900

    Add flag for server id offset.
    Signed-off-by: Kazuhito MATSUDA <kazuhito.matuda@gmail.com>

--- cmd/moco-conf-gen/cmd/root.go
+++ cmd/moco-conf-gen/cmd/root.go
@@ -3,7 +3,14 @@ package cmd
 import (
 	"github.com/cybozu-go/well"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+const (
+	serverIDOffsetFlag = "server-id-offset"
+)
+
+var serverIDOffset uint32
 
 var (
 	rootCmd = &cobra.Command{
@@ -22,10 +29,20 @@ var (
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			serverIDOffset = viper.GetUint32(serverIDOffsetFlag)
 			return subMain()
 		},
 	}
 )
+
+func init() {
+	// ordinal should be increased by 1000 as default because the case server-id is 0 is not suitable for the replication purpose
+	rootCmd.Flags().Uint32(serverIDOffsetFlag, 1000, "Offset value of server-id.")
+	err := viper.BindPFlags(rootCmd.Flags())
+	if err != nil {
+		panic(err)
+	}
+}
 
 // Execute executes the root command.
 func Execute() error {
